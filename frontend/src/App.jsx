576958aabf268092e0173e5eda50e726847ae20b
Stanislaw Pilch <stanislawpilch2002@gmail.com>
2024-01-10 17:23:52 +0100

    added text area

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [isConnectDisabled, setConnectDisabled] = useState(false);
   const [disabledButtons, setDisabledButtons] = useState([]);
   const [biddingArray, setBiddingArray] = useState([]);
+  const [consoleMessages, setConsoleMessages] = useState([]);
 
   const handleConnect = () => {
     const socket = new WebSocket('ws://localhost:3000');
@@ -13,39 +14,37 @@ function App() {
     });
 
     socket.addEventListener('message', function (event) {
+      const newConsoleMessages = [...consoleMessages, event.data];
+      setConsoleMessages(newConsoleMessages);
       console.log(event.data);
     });
-
   };
 
   const handleClick = (buttonNumber) => {
     const disabledRange = Array.from({ length: buttonNumber }, (_, index) => index + 1);
     const newBiddingArray = [...biddingArray];
-    newBiddingArray.push(document.getElementById('bid'+buttonNumber).innerText)
-    setBiddingArray(newBiddingArray)
+    newBiddingArray.push(document.getElementById('bid' + buttonNumber).innerText);
+    setBiddingArray(newBiddingArray);
     setDisabledButtons(disabledRange);
   };
 
   const handleReset = () => {
     setDisabledButtons([]);
-    setBiddingArray([])
+    setBiddingArray([]);
     setConnectDisabled(false);
+    setConsoleMessages([]);
   };
 
   const buttonNumbers = Array.from({ length: 35 }, (_, index) => index + 1);
-  const buttonNames = ['C', 'D', 'H', 'S', 'NT']
-
-  // String.fromCharCode(9827), // clubs
-  // String.fromCharCode(9830), // diamond
-  // String.fromCharCode(9829), // hearts
-  // String.fromCharCode(9824), // spades
+  const buttonNames = ['C', 'D', 'H', 'S', 'NT'];
 
   return (
     <>
       <div id='box' style={{ display: 'flex', flexWrap: 'wrap' }}>
         {buttonNumbers.map((number) => (
-          <button className='bid'
-            id={'bid'+number}
+          <button
+            className='bid'
+            id={'bid' + number}
             key={number}
             onClick={() => handleClick(number)}
             disabled={disabledButtons.includes(number)}
@@ -55,12 +54,18 @@ function App() {
         ))}
       </div>
       <div>
-        <button onClick={handleConnect} disabled={isConnectDisabled}>
-          Connect
+        <button style = {{margin: '1%'}} onClick={handleConnect} disabled={isConnectDisabled}>
+          Check
         </button>
-        <button onClick={handleReset}>
-          Reset
-        </button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+      <div>
+        <textarea
+          id='textArea'
+          value={consoleMessages.join('\n')}
+          readOnly
+          style={{ marginTop: '10px' }}
+        ></textarea>
       </div>
     </>
   );
